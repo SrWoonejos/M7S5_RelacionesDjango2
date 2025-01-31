51e1a01659a5d8dcd15b7038f75d0c93d1310e34
SrWoonejos <danmiranda@gmail.com>
2025-01-31 10:42:48 -0300

    update desafio

--- fabrica/productos/models.py
+++ fabrica/productos/models.py
@@ -8,6 +8,8 @@ class Manufacturer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='', null=True)  
+    price = models.DecimalField(max_digits=10, default=0, decimal_places=1)  
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
 
     def __str__(self):
